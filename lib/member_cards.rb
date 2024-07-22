6838d1ccdf93d45663aa84a1410a7cdba96cba78
Itamar <ihassin@gmail.com>
2024-07-22 08:16:38 -0400

    Optimised

--- lib/member_cards.rb
+++ lib/member_cards.rb
@@ -3,16 +3,11 @@
 class MemberCards
   def self.get_cards(trello_board:, for_member_id:, board_id:)
     trello_board.find(board_id).cards.select do |card|
-      in_progress?(participants: card.member_ids,
-                   completed: card.due_complete,
-                   list_name: card.list.name,
-                   for_member_id: for_member_id)
+      card.member_ids.include?(for_member_id) && in_progress?(completed: card.due_complete, list_name: card.list.name)
     end
   end
 
-  def self.in_progress?(participants:, completed:, list_name:, for_member_id:)
-    participants.include?(for_member_id) &&
-      !completed &&
-      !list_name.downcase.include?('done')
+  def self.in_progress?(completed:, list_name:)
+    !completed && !list_name.downcase.include?('done')
   end
 end
